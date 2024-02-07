48bae947ce2aaae96c24a0f445314c2ee0b42493
Piotr Patrzyk <piotr@patrzyk.me>
2024-02-07 15:01:08 +0100

    init swarm stacks; readonly users

--- docker/conf_clickhouse/init-db.sql
+++ docker/conf_clickhouse/init-db.sql
@@ -5,7 +5,7 @@ CREATE TABLE wiki_stream (
     created_at DateTime64(4, 'Etc/UTC'),
     data_raw_json String
 ) ENGINE = RabbitMQ SETTINGS 
-    rabbitmq_address = 'amqp://user_666:password_666@rabbit:5672',
+    rabbitmq_address = 'amqp://rabbitro:rabbitro_pass@rabbit:5672',
     rabbitmq_queue_consume = true,
     rabbitmq_queue_base = 'wikiqueue',
     rabbitmq_format = 'JSONEachRow',
@@ -22,3 +22,6 @@ ORDER BY created_at;
 
 CREATE MATERIALIZED VIEW wiki_stream_mv TO wiki_raw
 AS SELECT generateUUIDv4(), created_at, data_raw_json FROM wiki_stream;
+
+create user clickhousero identified with sha256_password by 'clickhousero_pass' settings profile 'readonly';
+grant select on wiki.* to clickhousero;
